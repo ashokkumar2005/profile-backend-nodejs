266e7a9292dae28f5bb2c66f25b99e03b881287d
Ashokkumar T <136552833+ashokkumar2005@users.noreply.github.com>
2026-04-06 14:07:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
+console.log("CLIENT_URL:", process.env.CLIENT_URL);
+console.log("Allowed Origins:", allowedOrigins);
 
 app.use(cors({
   origin: (origin, callback) => {
